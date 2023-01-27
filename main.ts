7143181024e7ff67cf1ee674c1cc53859adc763a
Bjarno Oeyen <git@bjarno.be>
2023-01-27 15:17:19 +0100

    Prototype of plugin

--- main.ts
+++ main.ts
@@ -1,81 +1,241 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownView, Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export default class BackSlashLatexMacroPlugin extends Plugin {
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	readonly REPLACE = {
+		"Downarrow": "⇓",
+		"nwarrow": "↖",
+		"downarrow": "↓",
+		"Rightarrow": "⇒",
+		"rightarrow": "→",
+		"rightharpoonup": "⇀",
+		"leftharpoonup": "↼",
+		"rightharpoondown": "⇁",
+		"leftharpoondown": "↽",
+		"rightleftharpoons": "⇌",
+		"mapsto": "↦",
+		"searrow": "↘",
+		"swarrow": "↙",
+		"leftarrow": "←",
+		"uparrow": "↑",
+		"Leftarrow": "⇐",
+		"longrightarrow": "⟶",
+		"Uparrow": "⇑",
+		"Leftrightarrow": "⇔",
+		"updownarrow": "↕",
+		"leftrightarrow": "↔",
+		"nearrow": "↗",
+		"Updownarrow": "⇕",
+		"leadsto": "↝",
+		"aleph": "א",
+		"prime": "′",
+		"emptyset": "∅",
+		"nabla": "∇",
+		"diamondsuit": "♦",
+		"spadesuit": "♠",
+		"clubsuit": "♣",
+		"heartsuit": "♥",
+		"sharp": "♯",
+		"flat": "♭",
+		"natural": "♮",
+		"surd": "√",
+		"neg": "¬",
+		"triangle": "△",
+		"forall": "∀",
+		"all": "∀",
+		"exists": "∃",
+		"infty": "∞",
+		"circ": "∘",
+		"partial": "∂",
+		"alpha": "α",
+		"theta": "θ",
+		"tau": "τ",
+		"beta": "β",
+		"vartheta": "ϑ",
+		"pi": "π",
+		"upsilon": "υ",
+		"gamma": "γ",
+		"varpi": "ϖ",
+		"phi": "ϕ",
+		"delta": "δ",
+		"kappa": "κ",
+		"rho": "ρ",
+		"varphi": "φ",
+		"epsilon": "ϵ",
+		"lambda": "λ",
+		"varrho": "ϱ",
+		"chi": "χ",
+		"varepsilon": "ε",
+		"mu": "μ",
+		"sigma": "σ",
+		"psi": "ψ",
+		"zeta": "ζ",
+		"nu": "ν",
+		"varsigma": "ς",
+		"omega": "ω",
+		"eta": "η",
+		"xi": "ξ",
+		"iota": "ι",
+		"Gamma": "Γ",
+		"Lambda": "Λ",
+		"Sigma": "Σ",
+		"Psi": "Ψ",
+		"Delta": "Δ",
+		"Xi": "Ξ",
+		"Upsilon": "Υ",
+		"Omega": "Ω",
+		"Theta": "Θ",
+		"Pi": "Π",
+		"Phi": "Φ",
+		"pm": "±",
+		"cap": "∩",
+		"diamond": "◇",
+		"lozenge": "◊",
+		"square": "□",
+		"oplus": "⊕",
+		"mp": "∓",
+		"cup": "∪",
+		"bigtriangleup": "△",
+		"ominus": "⊖",
+		"times": "×",
+		"uplus": "⊎",
+		"bigtriangledown": "▽",
+		"otimes": "⊗",
+		"div": "÷",
+		"sqcap": "⊓",
+		"triangleright": "▹",
+		"oslash": "⊘",
+		"ast": "∗",
+		"sqcup": "⊔",
+		"vee": "∨",
+		"wedge": "∧",
+		"lor": "∨",
+		"land": "∧",
+		"lnot": "¬",
+		"triangleleft": "◃",
+		"odot": "⊙",
+		"star": "★",
+		"dagger": "†",
+		"bullet": "•",
+		"ddagger": "‡",
+		"wr": "≀",
+		"amalg": "⨿",
+		"leq": "≤",
+		"geq": "≥",
+		"equiv": "≡",
+		"models": "⊨",
+		"prec": "≺",
+		"succ": "≻",
+		"precdot": "⋖",
+		"succdot": "⋗",
+		"sim": "∼",
+		"perp": "⊥",
+		"bot": "⊥",
+		"top": "⊤",
+		"preceq": "≼",
+		"succeq": "≽",
+		"simeq": "≃",
+		"ll": "≪",
+		"gg": "≫",
+		"asymp": "≍",
+		"parallel": "∥",
+		"subset": "⊂",
+		"supset": "⊃",
+		"approx": "≈",
+		"bowtie": "⋈",
+		"subseteq": "⊆",
+		"supseteq": "⊇",
+		"cong": "≅",
+		"sqsubsetb": "⊏",
+		"sqsupsetb": "⊐",
+		"neq": "≠",
+		"smile": "⌣",
+		"sqsubseteq": "⊑",
+		"sqsupseteq": "⊒",
+		"doteq": "≐",
+		"frown": "⌢",
+		"in": "∈",
+		"ni": "∋",
+		"notin": "∉",
+		"propto": "∝",
+		"vdash": "⊢",
+		"dashv": "⊣",
+		"cdot": "·",
+		"sum": "∑",
+		"prod": "∏",
+		"coprod": "∐",
+		"int": "∫",
+		"oint": "∮",
+		"sqrt": "√",
+		"skull": "☠",
+		"smiley": "☺",
+		"blacksmiley": "☻",
+		"frownie": "☹",
+		"S": "§",
+		"l": "ł",
+		"newpage": "^L",
+		"vdots": "⋮",
+		"ddots": "⋱",
+		"cdots": "⋯",
+		"hdots": "⋯",
+		"ldots": "…",
+		"langle": "⟨",
+		"rangle": "⟩",
+		"amp": "&",
+		"invamp": "⅋",
+		"multimap": "⊸",
+		"rightlollipop": "⊸",
+		"leftmultimap": "⟜",
+		"multimapinv": "⟜",
+		"leftlollipop": "⟜"
+	}
 
 	async onload() {
-		await this.loadSettings();
+		const platform = process.platform;
+		const insertModifier = (platform === 'darwin') ? "Meta" : "Ctrl";
+		const completeModifier = (platform === 'darwin') ? "Ctrl" : "Alt";
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: 'insert-lambda',
+			name: 'Add λ',
+			hotkeys: [
+			{modifiers: [insertModifier], key: "\\"},
+			],
+			editorCallback: () => {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (view) {
+					view.editor.replaceSelection("λ");
+				}
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
+		})
+
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			id: 'complete',
+			name: 'Complete character',
+			hotkeys: [
+			{modifiers: [completeModifier], key: "\\"},
+			],
+			editorCallback: () => {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (view) {
+					const cursor = view.editor.getCursor();
+					const line = view.editor.getLine(cursor.line);
+					const pos = cursor.ch;
+					const lineUtil = line.substring(0, pos);
+					const lastBackslash = lineUtil.lastIndexOf("\\");
+					if (lastBackslash === -1) { return; }
+					const keyWord = lineUtil.substring(lastBackslash + 1);
+					if (keyWord === "") { return; }
+					const replaceBy = this.REPLACE[keyWord];
+					if (replaceBy) {
+						const from = {line: cursor.line, ch: cursor.ch - keyWord.length - 1};
+						const to = cursor;
+						view.editor.replaceRange(replaceBy, from, to, null);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -88,50 +248,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
